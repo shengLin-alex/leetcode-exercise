8ebd8753eba68b69fadbece65203741e62368f60
shenglin-alex <a182820998@gmail.com>
2019-07-12 11:22:11 +0800

    0101 註解

--- 0094. Binary Tree Inorder Traversal/bt_inorder_trav.cpp
+++ 0094. Binary Tree Inorder Traversal/bt_inorder_trav.cpp
@@ -16,30 +16,39 @@
 #include <stack>
 #include "../tree_helper.hpp"
 
-// recursive:
-// vector<int> res;
-// vector<int> inorderTraversal(TreeNode* root) {
-//     if (root != nullptr) {
-//         inorderTraversal(root->left);
-//         res.push_back(root->val);
-//         inorderTraversal(root->right);
-//     }
-        
-//     return res;
-// }
+// inorder 就是 dfs 然後優先走left
 
+// recursive:
+std::vector<int> res;
+std::vector<int> inorderTraversal(TreeNode* root) {
+    if (root != nullptr) {
+        inorderTraversal(root->left);
+        res.push_back(root->val);
+        inorderTraversal(root->right);
+    }
+
+    return res;
+}
+
+// 迭代
 std::vector<int> inorderTraversal(TreeNode* root) {
     std::vector<int> result;
     std::stack<TreeNode*> tempStack;
     while (!tempStack.empty() || root != nullptr) {
+
+        // 在 root != nullptr 時持續先往左邊 search
         if (root != nullptr) {
             tempStack.push(root);
             root = root->left;
         }
         else {
+            // root 為 nullptr 後，開使依序從 stack 拿出 node
+            // 由於 stack 的特性會優先取得最深的 left node
             root = tempStack.top();
             result.push_back((tempStack.top())->val);
             tempStack.pop();
+
+            // 根據 inorder 將root 設為 right 往右繼續搜尋
             root = root->right;
         }
     }
--- 0101. Symmetric Tree/symmetric_tree.cpp
+++ 0101. Symmetric Tree/symmetric_tree.cpp
@@ -27,6 +27,7 @@
 
 using namespace std;
 
+// 利用 dfs 來紀錄一個node 的 val 和 是屬於 右邊還是左邊(label)
 void traversal(TreeNode *root, vector<pair<int,int>> &tmp, int label)
 {
     if (root == nullptr) return;
@@ -41,11 +42,13 @@ bool isSymmetric(TreeNode *root)
     vector<pair<int,int>> tmp;
     traversal(root, tmp, 0);
 
+    // dfs 後取所有node 的值與左右label
+    // 用 two pointers 進行比較
     int st = 0, ed = tmp.size() - 1;
     while (st < ed)
     {
-        if (tmp[st].first != tmp[ed].first) return false;
-        if (tmp[st].second == tmp[ed].second) return false;
+        if (tmp[st].first != tmp[ed].first) return false; // val 必須一樣
+        if (tmp[st].second == tmp[ed].second) return false; // label 必須相反
         st++;
         ed--;
     }
