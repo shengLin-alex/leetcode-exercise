0add49ad62e4ab42018faa395db265eb780e6d9e
shenglin-alex <a182820998@gmail.com>
2019-08-14 09:47:43 +0800

    finish 0033

--- 0033. Search in Rotated Sorted Array/search_in_rotated_sorted_array.cpp
+++ 0033. Search in Rotated Sorted Array/search_in_rotated_sorted_array.cpp
@@ -19,6 +19,40 @@
 
 #include <vector>
 
-int search(std::vector<int> &nums, int target) {
+// 0    1    2    4    5    6    7
 
+// 7    0    1    2    4    5    6
+
+// 6    7    0    1    2    4    5
+
+// 5    6    7    0    1    2    4
+
+// 4    5    6    7    0    1    2
+
+// 2    4    5    6    7    0    1
+
+// 1    2    4    5    6    7    0
+// 觀察上表可以發現，中點若小於最右邊的數，則[mid, right] 有序
+// 反之，[left, mid] 有序
+int search(std::vector<int> &nums, int target) {
+  int left = 0, right = nums.size() - 1;
+  while (left <= right) {
+    int mid = left + (right - left) / 2;
+    if (nums[mid] == target)
+      return mid;
+
+    if (nums[mid] < nums[right]) {                     // 右半有序
+      if (nums[mid] < target && nums[right] >= target) // 找右邊
+        left = mid + 1;
+      else // 找左邊
+        right = mid - 1;
+    } else {                                          // 左半有序
+      if (nums[mid] > target && nums[left] <= target) // 找左邊
+        right = mid - 1;
+      else // 找右邊
+        left = mid + 1;
+    }
+  }
+
+  return -1;
 }
