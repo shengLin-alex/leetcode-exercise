1ee3af83c34212f752896040c40d11ca97229bcc
alex <a182820998@gmail.com>
2021-06-14 11:00:33 +0800

    add 0516 1470 1518 & EightQueens

---
+++ GetMostVisited/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace GetMostVisited
+{
+  class Program
+    {
+        static void Main(string[] args)
+        {
+            var res = getMostVisited(10, new List<int>{ 9, 7, 3, 1 });
+
+            Console.WriteLine(res);
+        }
+
+        public static int getMostVisited(int n, List<int> sprints)
+        {
+            var map = new int[n];
+            var isBackwards = new List<bool>();
+            
+            for (var i = 0; i < sprints.Count; i++)
+                if (i + 1 < sprints.Count)
+                    isBackwards.Add(sprints[i] > sprints[i + 1]);
+
+            for (var i = 0; i < sprints.Count - 1; i++)
+            {
+                if (i + 1 < sprints.Count && isBackwards[i])
+                    for (var j = sprints[i]; j >= sprints[i + 1]; j--)
+                    {
+                        map[j] += 1;
+                    }
+                else if (i + 1 < sprints.Count && !isBackwards[i])
+                    for (var j = sprints[i]; j <= sprints[i + 1]; j++)
+                    {
+                        map[j] += 1;
+                    }
+            }
+
+            return map.ToList().IndexOf(map.Max());
+        }
+    }
+}
